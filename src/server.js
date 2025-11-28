6b87910febdf9fb41b66deb228cbdee824c6635d
Kirtan Nahar <119801478+notkirtann@users.noreply.github.com>
2025-11-28 11:08:15 +0530

    Integrate Swagger for API documentation
    Added Swagger documentation setup and updated imports.

--- src/server.js
+++ src/server.js
@@ -1,29 +1,33 @@
-// import dotenv from 'dotenv';
-// import { fileURLToPath } from 'url';
-// import path from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// dotenv.config({
-//   path: path.resolve(__dirname, '../.env'),
-//   processEnv: process.env,
-// });
 import dotenv from 'dotenv'
 dotenv.config()
 console.log("<-------------------------------------------------->Loaded MONGODB_URL:", process.env.MONGODB_URL);
-
 import express from "express";
-import "./db/mongoose.js";
+import "./config/mongoose.js";
 import userRoutes from "./routes/user.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+//swagger
+import { swaggerUi, swaggerDocument } from './config/swagger.js';
 
 const app = express();
 const PORT = process.env.PORT;
-
 app.use(express.json());
+
+//swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Task Manager API Documentation",
+  customfavIcon: "/favicon.ico"
+}));
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Task Manager API',
+    documentation: '/api-docs'
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`<-------------------------------------------------->Server running on port ${PORT}`);
